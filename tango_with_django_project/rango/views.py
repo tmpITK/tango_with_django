cb2e31a1931caf718c9a3c3be04d3abbbdf6757f
Unknown <torok.mark.patrik@gmail.com>
2018-06-21 19:36:47 +0200

    URLS
    New url to about page.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner! About: http://127.0.0.1:8000/rango/about")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. Main: http://127.0.0.1:8000/rango/")
