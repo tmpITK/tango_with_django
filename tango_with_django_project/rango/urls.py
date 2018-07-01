ce4f295b087cfb06791d508d7eaa7c93e5b06f69
Unknown <torok.mark.patrik@gmail.com>
2018-07-01 10:04:23 +0200

    Chapter 12

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -10,7 +10,4 @@ urlpatterns = [
     
     url(r'category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name="add_page"),
 
-    url(r'^register/$', views.register, name='register'),
-    url(r'^login/$',views.user_login, name='login'),
-    url(r'^logout/$', views.user_logout, name='logout'),
 ]
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -127,5 +128,9 @@ USE_TZ = True
 STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
 
+
+REGISTRATION_OPEN = True
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
 LOGIN_REDIRECT_URL = '/rango/'
-LOGIN_URL = '/rango/login/'
+LOGIN_URL = '/accounts/login/'
